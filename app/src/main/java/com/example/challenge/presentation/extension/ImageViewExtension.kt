28efa911b2d4fba97330c1d77bec6a21e5f9a4fc
Vyceros <luka.kurashvili.1@btu.edu.ge>
2025-03-19 11:49:33 +0400

    Final

--- app/src/main/java/com/example/challenge/presentation/extension/ImageViewExtension.kt
+++ app/src/main/java/com/example/challenge/presentation/extension/ImageViewExtension.kt
@@ -1,13 +1,14 @@
 package com.example.challenge.presentation.extension
 
-import androidx.appcompat.widget.AppCompatImageView
+import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.example.challenge.R
 
 
-fun AppCompatImageView.loadImage(url: String?) {
+fun ImageView.loadImage(url: String?) {
     Glide.with(context)
         .load(url)
+        .error(R.drawable.ic_launcher_background)
         .placeholder(R.drawable.ic_launcher_background)
-        .into(this);
+        .into(this)
 }
